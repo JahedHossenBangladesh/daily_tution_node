1a54f9bbf7b1dbb75bafcf71cb704ac969f38889
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-09-15 23:40:45 +0600

    event loop

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const car ={
-    brand:'BMW',
-    model: 'x5'
+// const car ={
+//     brand:'BMW',
+//     model: 'x5'
+// }
+// console.log(car)
+// module.exports = car;
+const bar = () =>{
+    console.log('bar')
 }
+const baz = () =>{
+    console.log('baz')
+}
+const foo = () =>{
+    console.log('foo');
+    bar();
+    baz();
+}
+foo();
 
-module.exports = car;
+const foo2 = () =>{
+    console.log('foo2')
+    setTimeout(bar,1000);
+    baz();
+}
+foo2();    //console.log('foo2')
