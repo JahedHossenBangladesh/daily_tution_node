768767ee310c1564461f3683beec3fde2c55a1d6
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-09-16 01:58:01 +0600

    This is Call back

--- index.js
+++ index.js
@@ -4,22 +4,34 @@
 // }
 // console.log(car)
 // module.exports = car;
-const bar = () =>{
-    console.log('bar')
-}
-const baz = () =>{
-    console.log('baz')
-}
-const foo = () =>{
-    console.log('foo');
-    bar();
-    baz();
-}
-foo();
+// const bar = () =>{
+//     console.log('bar')
+// }
+// const baz = () =>{
+//     console.log('baz')
+// }
+// const foo = () =>{
+//     console.log('foo');
+//     bar();
+//     baz();
+// }
+// foo();
 
-const foo2 = () =>{
-    console.log('foo2')
-    setTimeout(bar,1000);
-    baz();
+// const foo2 = () =>{
+//     console.log('foo2')
+//     setTimeout(bar,1000);
+//     baz();
+// }
+// foo2();    //console.log('foo2')
+
+function getMessage(msg,callback){
+    setTimeout( () =>{
+        console.log(msg);
+        callback();
+    },1000);
 }
-foo2();    //console.log('foo2')
+function displayMessage(){
+    console.log('displayMessage');
+}
+
+getMessage('hello',displayMessage);
