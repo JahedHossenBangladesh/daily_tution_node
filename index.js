eeb6cabde6a1661d66cb92e273b137caecb191d2
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-09-17 10:44:52 +0600

    Http server

--- index.js
+++ index.js
@@ -53,27 +53,41 @@
 // function afterMsg(){
 //     console.log('Print After');
 // }
-function Clown(){
-    return new Promise(resolve =>{
-        setTimeout(() =>{
-            resolve('Hello')
-        },2000)
-    })
-}
-async  function msg(callback ){
-    const msg = await Clown();
-    console.log(msg);
-    callback();
-}
-
-msg(getResult)
-
-function getResult(){
-    console.log('After the Promise')
-}
 
 
+// function Clown(){
+//     return new Promise(resolve =>{
+//         setTimeout(() =>{
+//             resolve('Hello')
+//         },2000)
+//     })
+// }
+// async  function msg(callback ){
+//     const msg = await Clown();
+//     console.log(msg);
+//     callback();
+// }
 
+// msg(getResult)
+
+// function getResult(){
+//     console.log('After the Promise')
+// }
+
+// Http server
+
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+http.createServer((req,res) =>{
+
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.write('welcome to http')
+    res.end();
+}).listen(port,hostname,() =>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
 
 
 
