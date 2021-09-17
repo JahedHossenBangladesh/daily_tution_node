95b90e8049b1f3124b06ec0cb9dd394ba0d5c9da
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-09-17 10:22:00 +0600

    async await

--- index.js
+++ index.js
@@ -37,19 +37,41 @@
 // getMessage('hello',displayMessage);
 
 
-let promise = new Promise(function(resolve,reject){
+// let promise = new Promise(function(resolve,reject){
 
-    setTimeout(()=> resolve("Run Before"),1000)
-})
+//     setTimeout(()=> resolve("Run Before"),1000)
+// })
 
-promise.then(
-    result => {
-        console.log(result)
-        afterMsg()
-    },
-    error => console.log(error)
-)
+// promise.then(
+//     result => {
+//         console.log(result)
+//         afterMsg()
+//     },
+//     error => console.log(error)
+// )
 
-function afterMsg(){
-    console.log('Print After');
+// function afterMsg(){
+//     console.log('Print After');
+// }
+function Clown(){
+    return new Promise(resolve =>{
+        setTimeout(() =>{
+            resolve('Hello')
+        },2000)
+    })
 }
+async  function msg(){
+    const msg = await Clown();
+    console.log(msg);
+}
+
+msg()
+
+
+
+
+
+
+
+
+
